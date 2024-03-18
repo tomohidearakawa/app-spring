18bd7508534525273da870274fe3783adb7350b5
tomohidearakawa <tomohide.arakawa@zozo.com>
2024-03-18 16:08:32 +0900

    output test JSON

--- SampleBootApp1/src/main/java/com/example/sample1pp/SampleBootApp1Application.java
+++ SampleBootApp1/src/main/java/com/example/sample1pp/SampleBootApp1Application.java
@@ -10,28 +10,58 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class SampleBootApp1Application {
 
-	String[][] data = {
-			{ "noname", "no email address", "0" },
-			{ "taro", "taro@yamada", "39" },
-			{ "hanako", "hanako@flower", "28" },
-			{ "sachiko", "sachiko@happy", "17" },
-			{ "jiro", "jiro@change", "6" }
+	DataObject[] data = {
+			new DataObject("noname", "no email address", 0),
+			new DataObject("taro", "taro@yamada", 39),
+			new DataObject("hanako", "hanako@flower", 28),
+			new DataObject("sachiko", "sachiko@happy", 17),
+			new DataObject("jiro", "jiro@change", 6)
 	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
 	}
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello, Spring Boot 3!!!";
+	@RequestMapping("/{num}")
+	public DataObject index(@PathVariable int num) {
+		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
+		return data[n];
+	}
+}
+
+class DataObject {
+	private String name;
+	private String mail;
+	private int age;
+
+	public DataObject(String name, String mail, int age) {
+		super();
+		this.name = name;
+		this.mail = mail;
+		this.age = age;
 	}
 
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num) {
-		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
-		String[] item = data[n];
-		String msg = "ID=%s. {name: %s, mail: %s, age: %s}";
-		return String.format(msg, num, item[0], item[1], item[2]);
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 }
