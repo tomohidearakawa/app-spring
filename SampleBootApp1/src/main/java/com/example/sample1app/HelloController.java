3f0142030a7498f8fa531513b6e6ac491188cd5d
tomohidearakawa <tomohide.arakawa@zozo.com>
2024-03-18 21:56:03 +0900

    fix build.gradle

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -2,12 +2,25 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("msg", "トップページ");
+        return "index";
+    }
+
+    @RequestMapping("/{num}")
+    public String index(@PathVariable int num, Model model) {
+        int res = 0;
+        for (int i = 1; i <= num; i++) {
+            res += i;
+        }
+        model.addAttribute("msg", "test: " + res);
         return "index";
     }
 }
