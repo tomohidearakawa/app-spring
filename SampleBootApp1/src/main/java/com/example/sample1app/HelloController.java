b0d4fa94a921618662c67aa193a382ff8b50bff7
tomohidearakawa <tomohide.arakawa@zozo.com>
2024-03-20 17:41:37 +0900

    add update, delete

--- SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
+++ SampleBootApp1/src/main/java/com/example/sample1app/HelloController.java
@@ -1,47 +1,106 @@
 package com.example.sample1app;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.sample1app.repositories.PersonRepository;
+
+import jakarta.transaction.Transactional;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index(ModelAndView mav) {
+    @Autowired
+    PersonRepository repository;
+
+    @PostConstruct
+    public void init() {
+        // １つ目のダミーデータ作成
+        Person p1 = new Person();
+        p1.setName("taro");
+        p1.setAge(39);
+        p1.setMail("taro@yamada");
+        repository.saveAndFlush(p1);
+        // ２つ目のダミーデータ作成
+        Person p2 = new Person();
+        p2.setName("hanako");
+        p2.setAge(28);
+        p2.setMail("hanako@flower");
+        repository.saveAndFlush(p2);
+        // ３つ目のダミーデータ作成
+        Person p3 = new Person();
+        p3.setName("sachiko");
+        p3.setAge(17);
+        p3.setMail("sachico@happy");
+        repository.saveAndFlush(p3);
+    }
+
+    @RequestMapping("/")
+    public ModelAndView index(
+            @ModelAttribute("formModel") Person Person,
+            ModelAndView mav) {
         mav.setViewName("index");
-        mav.addObject("msg", "フォームを送信下さい。");
+        mav.addObject("title", "Hello page");
+        mav.addObject("msg", "this is JPA sample data.");
+        List<Person> list = repository.findAll();
+        mav.addObject("data", list);
         return mav;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
+    @Transactional
     public ModelAndView form(
-            @RequestParam(value = "check1", required = false) boolean check1,
-            @RequestParam(value = "radio1", required = false) String radio1,
-            @RequestParam(value = "select1", required = false) String select1,
-            @RequestParam(value = "select2", required = false) String[] select2,
+            @ModelAttribute("formModel") Person Person,
             ModelAndView mav) {
+        repository.saveAndFlush(Person);
+        return new ModelAndView("redirect:/");
+    }
 
-        String res = "";
-        try {
-            res = "check:" + check1 +
-                    " radio:" + radio1 +
-                    " select:" + select1 +
-                    "\nselect2:";
-        } catch (NullPointerException e) {
-        }
-        try {
-            res += select2[0];
-            for (int i = 1; i < select2.length; i++) {
-                res += ", " + select2[i];
-            }
-        } catch (NullPointerException e) {
-            res += "null";
-        }
-        mav.addObject("msg", res);
-        mav.setViewName("index");
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView edit(@ModelAttribute Person Person,
+            @PathVariable int id, ModelAndView mav) {
+        mav.setViewName("edit");
+        mav.addObject("title", "edit Person.");
+        Optional<Person> data = repository.findById((long) id);
+        mav.addObject("formModel", data.get());
         return mav;
     }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @Transactional
+    public ModelAndView update(@ModelAttribute Person Person,
+            ModelAndView mav) {
+        repository.saveAndFlush(Person);
+        return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView delete(@PathVariable int id,
+            ModelAndView mav) {
+        mav.setViewName("delete");
+        mav.addObject("title", "Delete Person.");
+        mav.addObject("msg", "Can I delete this record?");
+        Optional<Person> data = repository.findById((long) id);
+        mav.addObject("formModel", data.get());
+        return mav;
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    @Transactional
+    public ModelAndView remove(@RequestParam long id,
+            ModelAndView mav) {
+        repository.deleteById(id);
+        return new ModelAndView("redirect:/");
+    }
+
 }
---
+++ SampleBootApp1/src/main/java/com/example/sample1app/repositories/PersonRepository.java
@@ -0,0 +1,14 @@
+package com.example.sample1app.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.sample1app.Person;
+
+@Repository
+public interface PersonRepository
+        extends JpaRepository<Person, Long> {
+    public Optional<Person> findById(Long name);
+}
