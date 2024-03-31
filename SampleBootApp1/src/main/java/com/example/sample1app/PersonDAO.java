029bf7c0dfc555a8c4d634641ef089ea629efc64
tomohidearakawa <tomohide.arakawa@zozo.com>
2024-03-31 17:35:01 +0900

    add dao findbyname

---
+++ SampleBootApp1/src/main/java/com/example/sample1app/PersonDAO.java
@@ -0,0 +1,14 @@
+package com.example.sample1app;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface PersonDAO<T> extends Serializable {
+
+    public List<T> getAll();
+
+    public T findById(long id); // ☆
+
+    public List<T> findByName(String name);
+
+}
